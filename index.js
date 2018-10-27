9ef1b68cc9dd22e3a29d52a70af7f52ec0def423
serpentz <absalomcherinet@gmail.com>
2018-10-27 12:31:32 -0400

    Done.

--- index.js
+++ index.js
@@ -7,3 +7,13 @@ You should NOT try to redefine any of the above variables.
 */
 
 // Write your code below this line!
+let num = 1000000000
+let newID = num + oldID
+const isAgeValid = function (age){ return Number.isInteger(age) }
+let ageIsValid = isAgeValid(currentAge) // we can also do Number.isInteger(currentAge)
+const randmonGenerator = function (range){ return Math.random()*range }
+let randomNumber = randmonGenerator(20)
+let randomInteger = Math.floor(randomNumber)
+console.log(randomInteger)
+let randomUserID = num + randomInteger
+console.log(randomUserID)
